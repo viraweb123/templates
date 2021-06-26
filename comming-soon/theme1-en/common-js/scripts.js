850ed81cac09ec4136bc718608bc771e7def89d1
hadi2 <hadi2@localhost>
2021-06-26 10:07:38 +0430

    some comming-soon templates are added

--- comming-soon/theme1-en/common-js/scripts.js
+++ comming-soon/theme1-en/common-js/scripts.js
@@ -1,15 +1,15 @@
 (function($){"use strict";if($.isFunction($.fn.fluidbox)){$('a').fluidbox();}
 if(isExists('#rounded-countdown')){var remainingSec=$('.countdown').data('remaining-sec');$('.countdown').ClassyCountdown({theme:"flat-colors-very-wide",end:$.now()+remainingSec});}
 if(isExists('#normal-countdown')){var date=$('#normal-countdown').data('date');$('#normal-countdown').countdown(date,function(event){var $this=$(this).html(event.strftime(''
-+'<div class="time-sec"><h3 class="main-time">%D</h3> <span>روز</span></div>'
-+'<div class="time-sec"><h3 class="main-time">%H</h3> <span>ساعت</span></div>'
-+'<div class="time-sec"><h3 class="main-time">%M</h3> <span>دقیقه</span></div>'
-+'<div class="time-sec"><h3 class="main-time">%S</h3> <span>ثانیه</span></div>'));});}
++'<div class="time-sec"><h3 class="main-time">%D</h3> <span>Days</span></div>'
++'<div class="time-sec"><h3 class="main-time">%H</h3> <span>Hours</span></div>'
++'<div class="time-sec"><h3 class="main-time">%M</h3> <span>Mins</span></div>'
++'<div class="time-sec"><h3 class="main-time">%S</h3> <span>Sec</span></div>'));});}
 $('a[href="#"]').on('click',function(event){return;});countdownTime();$('[data-nav-menu]').on('click',function(event){var $this=$(this),visibleHeadArea=$this.data('nav-menu');$(visibleHeadArea).toggleClass('visible');});var winWidth=$(window).width();dropdownMenu(winWidth);$(window).on('resize',function(){dropdownMenu(winWidth);});var isAnimated=false;})(jQuery);function countdownTime(){if(isExists('#clock')){$('#clock').countdown('2022/01/01',function(event){var $this=$(this).html(event.strftime(''
-+'<div class="time-sec"><span class="title">%D</span> روز </div>'
-+'<div class="time-sec"><span class="title">%H</span> ساعت </div>'
-+'<div class="time-sec"><span class="title">%M</span> دقیقه </div>'
-+'<div class="time-sec"><span class="title">%S</span> ثانیه </div>'));});}}
++'<div class="time-sec"><span class="title">%D</span> days </div>'
++'<div class="time-sec"><span class="title">%H</span> hours </div>'
++'<div class="time-sec"><span class="title">%M</span> minutes </div>'
++'<div class="time-sec"><span class="title">%S</span> seconds </div>'));});}}
 function dropdownMenu(winWidth){if(winWidth>767){$('.main-menu li.drop-down').on('mouseover',function(){var $this=$(this),menuAnchor=$this.children('a');menuAnchor.addClass('mouseover');}).on('mouseleave',function(){var $this=$(this),menuAnchor=$this.children('a');menuAnchor.removeClass('mouseover');});}else{$('.main-menu li.drop-down > a').on('click',function(){if($(this).attr('href')=='#')return false;if($(this).hasClass('mouseover')){$(this).removeClass('mouseover');}
 else{$(this).addClass('mouseover');}
 return false;});}}
